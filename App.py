835cb893be48fc206a03b7726407b8691237cd20
King-georges <opondigeorge@gmail.com>
2024-04-23 11:30:32 +0300

    Updating app

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title="Loan Default Prediction App",
+                page_icon= ":bar_chart:")
+st.title('Loan Default Prediction app')
+st.write("""
+## This app predicts the likelihood of defaulting on a loan.
+
+
+""")
